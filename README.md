67b1f2cbc9fdf3397ed091f7bc6fa0c541f9da74
Pierre <pierre.miniggio@gmail.com>
2019-12-09 22:28:51 +0100

    comment example & edit readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+Use that :
+```
+cp env.example.js env.js
+```
+
 Fill env.js with urls to use memo
+
+Install on Opera or Chrome
--- env.example.js
+++ env.example.js
@@ -1,2 +1,4 @@
-var getMemoUrl = '';
-var updateMemoUrl = '';
+// Both are GET requests
+var getMemoUrl = 'https://example.fr/memo/get';
+var updateMemoUrl = 'https://example.fr/memo/update';
+// ^ 'https://example.fr/memo/update?memo=content' Will be sent
