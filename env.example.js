ec17df02e8a3796e696149d1ac8b429e2648b526
Pierre MINIGGIO <=>
2020-03-06 16:45:11 +0100

    env is const

--- env.example.js
+++ env.example.js
@@ -1,4 +1,4 @@
 // Both are GET requests
-var getMemoUrl = 'https://example.fr/memo/get';
-var updateMemoUrl = 'https://example.fr/memo/update';
+const getMemoUrl = 'https://example.fr/memo/get';
+const updateMemoUrl = 'https://example.fr/memo/update';
 // ^ 'https://example.fr/memo/update?memo=content' Will be sent
