449142714e65f4355b04232d8074158432fa14b0
Pierre MINIGGIO <=>
2020-03-06 16:58:50 +0100

    passwords env

--- env.example.js
+++ env.example.js
@@ -1,4 +1,12 @@
 // Both are GET requests
-const getMemoUrl = 'https://example.fr/memo/get';
-const updateMemoUrl = 'https://example.fr/memo/update';
+const getMemoUrl = 'https://example.fr/memo/get'
+const updateMemoUrl = 'https://example.fr/memo/update'
 // ^ 'https://example.fr/memo/update?memo=content' Will be sent
+
+// Also GET requests
+const passwordLoginUrl = 'https://example.fr/passwords/login?creds=someloginkey' // Complete login url w/ infos
+const getPasswordsUrl = 'https://example.fr/passwords/get'
+// ^ Will be sent :
+// 'https://example.fr/passwords/get' // To pull all main folders
+// 'https://example.fr/passwords/get?folder=1' // To pull infos, content & breadcrumbs from folder 1
+// 'https://example.fr/passwords/get?file=1' // To pull file & parent folder & parent folder data ^
