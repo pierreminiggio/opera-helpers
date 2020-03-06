fa0312a3a6f954c60ce51e07ede142d7f9c36425
Pierre MINIGGIO <=>
2020-03-06 17:18:25 +0100

    add passwords button

--- js/memo.js
+++ js/memo.js
@@ -1,129 +1,129 @@
 if (getMemoUrl && updateMemoUrl) {
-	var lastMemoText = '';
+	var lastMemoText = ''
 
 	function autosize(elt) {
-			setTimeout(function() {
-				elt.style.height = 'auto';
-				elt.style.height = Math.min(elt.scrollHeight, 500) + 'px';
-			}, 0);
+		setTimeout(function() {
+			elt.style.height = 'auto'
+			elt.style.height = Math.min(elt.scrollHeight, 500) + 'px'
+		}, 0)
 	}
 
 	function fillMemo(elt) {
-		var txt = '';
-		var fileRequest = new XMLHttpRequest();
+		let txt = ''
+		const fileRequest = new XMLHttpRequest()
 		fileRequest.onreadystatechange = function() {
 			if(fileRequest.status == 200 && fileRequest.readyState == 4) {
-				txt = fileRequest.responseText;
-				elt.value = txt;
-				lastMemoText = txt;
+				txt = fileRequest.responseText
+				elt.value = txt
+				lastMemoText = txt
 			}
-		};
-		fileRequest.open('GET', getMemoUrl, true);
-		fileRequest.send();
+		}
+		fileRequest.open('GET', getMemoUrl, true)
+		fileRequest.send()
 	}
 
 	function updateMemo(value) {
-		var fileRequest = new XMLHttpRequest();
-		fileRequest.open('GET', updateMemoUrl+'?memo='+value, true);
-		fileRequest.send();
+		const fileRequest = new XMLHttpRequest()
+		fileRequest.open('GET', updateMemoUrl+'?memo='+value, true)
+		fileRequest.send()
 	}
 
 	(function() {
 
 		// Création memo
-		var memo = document.querySelector('#memo-pierre');
+		let memo = document.querySelector('#memo-pierre')
 		if (memo === null) {
-			memo = document.createElement('TEXTAREA');
-			memo.id = 'memo-pierre';
-			memo.style.position = 'fixed';
-			memo.style.top = '30%';
-			memo.style.right = '100px';
-			memo.style.width = '400px';
-			memo.style.zIndex = '999999';
-			memo.style.color = '#fff';
-			memo.style.backgroundColor = '#000';
-			memo.style.opacity = '.6';
-			memo.style.transition = '.5s';
-			memo.style.display = 'none';
-			document.body.appendChild(memo);
+			memo = document.createElement('TEXTAREA')
+			memo.id = 'memo-pierre'
+			memo.style.position = 'fixed'
+			memo.style.top = '30%'
+			memo.style.right = '100px'
+			memo.style.width = '400px'
+			memo.style.zIndex = '999999'
+			memo.style.color = '#fff'
+			memo.style.backgroundColor = '#000'
+			memo.style.opacity = '.6'
+			memo.style.transition = '.5s'
+			memo.style.display = 'none'
+			document.body.appendChild(memo)
 
 			// Events
 			memo.addEventListener('mouseover', function(e) {
-				this.style.opacity = '1';
-				memoTrigger.style.opacity = '1';
-			});
+				this.style.opacity = '1'
+				memoTrigger.style.opacity = '1'
+			})
 
 			memo.addEventListener('mouseleave', function(e) {
-				this.style.opacity = '.6';
-				memoTrigger.style.opacity = '.6';
-			});
+				this.style.opacity = '.6'
+				memoTrigger.style.opacity = '.6'
+			})
 
 			memo.addEventListener('keydown', function(e) {
-				autosize(this);
-			});
+				autosize(this)
+			})
 
 			memo.addEventListener('change', function(e) {
-				var encodedValue = encodeURIComponent(this.value);
+				const encodedValue = encodeURIComponent(this.value)
 				if (encodedValue !== lastMemoText) {
-					lastMemoText = encodedValue;
-					updateMemo(encodedValue);
+					lastMemoText = encodedValue
+					updateMemo(encodedValue)
 				}
 			})
 		}
 
 		// Création memo trigger
-		var memoTrigger = document.querySelector('#memo-trigger-pierre');
+		let memoTrigger = document.querySelector('#memo-trigger-pierre')
 		if (memoTrigger === null) {
 
-			memoTrigger = document.createElement('DIV');
+			memoTrigger = document.createElement('DIV')
 
 			// Properties
-			memoTrigger.id = 'memo-trigger-pierre';
-			memoTrigger.style.position = 'fixed';
-			memoTrigger.innerHTML = '🖉';
-			memoTrigger.style.textAlign = 'center';
-			memoTrigger.title = 'Memo';
-			memoTrigger.style.top = '30%';
-			memoTrigger.style.right = '20px';
-			memoTrigger.style.zIndex = '999999';
-			memoTrigger.style.color = '#fff';
-			memoTrigger.style.backgroundColor = '#000';
-			memoTrigger.style.height = '30px';
-			memoTrigger.style.lineHeight = '30px';
-			memoTrigger.style.width = '30px';
-			memoTrigger.style.fontSize = '20px';
-			memoTrigger.style.borderRadius = '50%';
-			memoTrigger.style.userSelect = 'none';
+			memoTrigger.id = 'memo-trigger-pierre'
+			memoTrigger.style.position = 'fixed'
+			memoTrigger.innerHTML = '🖉'
+			memoTrigger.style.textAlign = 'center'
+			memoTrigger.title = 'Memo'
+			memoTrigger.style.top = '30%'
+			memoTrigger.style.right = '20px'
+			memoTrigger.style.zIndex = '999999'
+			memoTrigger.style.color = '#fff'
+			memoTrigger.style.backgroundColor = '#000'
+			memoTrigger.style.height = '30px'
+			memoTrigger.style.lineHeight = '30px'
+			memoTrigger.style.width = '30px'
+			memoTrigger.style.fontSize = '20px'
+			memoTrigger.style.borderRadius = '50%'
+			memoTrigger.style.userSelect = 'none'
 			
-			memoTrigger.style.cursor = 'pointer';
-			memoTrigger.style.opacity = '.6';
-			memoTrigger.style.transition = '.5s';
+			memoTrigger.style.cursor = 'pointer'
+			memoTrigger.style.opacity = '.6'
+			memoTrigger.style.transition = '.5s'
 
 			// Events
 			memoTrigger.addEventListener('mouseover', function(e) {
-				this.style.opacity = '1';
-			});
+				this.style.opacity = '1'
+			})
 
 			memoTrigger.addEventListener('mouseleave', function(e) {
-				this.style.opacity = '.6';
-			});
+				this.style.opacity = '.6'
+			})
 
 			memoTrigger.addEventListener('click', function(e) {
-				var newDisplayState = 'none';
+				let newDisplayState = 'none'
 				if (memo.style.display === 'none') {
-					newDisplayState = 'block';
+					newDisplayState = 'block'
 				}
-				memo.style.display = newDisplayState;
+				memo.style.display = newDisplayState
 
 				if (newDisplayState === 'block') {
-					fillMemo(memo);
+					fillMemo(memo)
 					setTimeout(function() {
-						autosize(memo);
-					}, 500);
+						autosize(memo)
+					}, 500)
 				}
-			});
+			})
 
-			document.body.appendChild(memoTrigger);
+			document.body.appendChild(memoTrigger)
 		}
-	})();
+	})()
 }
---
+++ js/password.js
@@ -0,0 +1,99 @@
+if (passwordLoginUrl && getPasswordsUrl) {
+    
+    (function() {
+
+        function fillpassword(elt) {
+            password.innerHTML = 'Chargement...'
+            const fileRequest = new XMLHttpRequest()
+            fileRequest.onreadystatechange = function() {
+                if(fileRequest.status == 200 && fileRequest.readyState == 4) {
+                    password.innerHTML = fileRequest.responseText
+                }
+            }
+            fileRequest.open('GET', passwordLoginUrl, true)
+            fileRequest.send()
+        }
+
+        // Création password
+		let password = document.querySelector('#password-pierre')
+		if (password === null) {
+			password = document.createElement('div')
+			password.id = 'password-pierre'
+			password.style.position = 'fixed'
+			password.style.top = '30%'
+			password.style.right = '100px'
+			password.style.width = '400px'
+			password.style.zIndex = '999999'
+			password.style.color = '#fff'
+			password.style.backgroundColor = '#000'
+			password.style.opacity = '.6'
+			password.style.transition = '.5s'
+			password.style.display = 'none'
+			document.body.appendChild(password)
+
+			// Events
+			password.addEventListener('mouseover', function(e) {
+				this.style.opacity = '1'
+				passwordTrigger.style.opacity = '1'
+			})
+
+			password.addEventListener('mouseleave', function(e) {
+				this.style.opacity = '.6'
+				passwordTrigger.style.opacity = '.6'
+			})
+		}
+
+        // Création password trigger
+		let passwordTrigger = document.querySelector('#password-trigger-pierre')
+		if (passwordTrigger === null) {
+
+			passwordTrigger = document.createElement('DIV')
+
+			// Properties
+			passwordTrigger.id = 'password-trigger-pierre'
+			passwordTrigger.style.position = 'fixed'
+			passwordTrigger.innerHTML = '🔑'
+			passwordTrigger.style.textAlign = 'center'
+			passwordTrigger.title = 'Mots de passe'
+			passwordTrigger.style.top = '35%'
+			passwordTrigger.style.right = '20px'
+			passwordTrigger.style.zIndex = '999999'
+			passwordTrigger.style.color = '#fff'
+			passwordTrigger.style.backgroundColor = '#000'
+			passwordTrigger.style.height = '30px'
+			passwordTrigger.style.lineHeight = '30px'
+			passwordTrigger.style.width = '30px'
+			passwordTrigger.style.fontSize = '20px'
+			passwordTrigger.style.borderRadius = '50%'
+			passwordTrigger.style.userSelect = 'none'
+			
+			passwordTrigger.style.cursor = 'pointer'
+			passwordTrigger.style.opacity = '.6'
+			passwordTrigger.style.transition = '.5s'
+
+			// Events
+			passwordTrigger.addEventListener('mouseover', function(e) {
+				this.style.opacity = '1'
+			})
+
+			passwordTrigger.addEventListener('mouseleave', function(e) {
+				this.style.opacity = '.6'
+			})
+
+			passwordTrigger.addEventListener('click', function(e) {
+				let newDisplayState = 'none'
+				if (password.style.display === 'none') {
+					newDisplayState = 'block'
+				}
+				password.style.display = newDisplayState
+
+				if (newDisplayState === 'block') {
+					fillpassword(password)
+				}
+			})
+
+			document.body.appendChild(passwordTrigger)
+		}
+    })()
+
+}
